0cf801e315051306f50a0ab58b397eb8cd00cb26
XuPeiYao <st89382000@gmail.com>
2020-08-17 00:59:25 +0800

    feat: 移除必要的命名空間引用

--- src/XPY.WebsiteSolution.Web/Controllers/SampleController.cs
+++ src/XPY.WebsiteSolution.Web/Controllers/SampleController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Autofac.Extras.DynamicProxy;
-
 using Microsoft.AspNetCore.Mvc; 
 
 using XPY.WebsiteSolution.Models;
