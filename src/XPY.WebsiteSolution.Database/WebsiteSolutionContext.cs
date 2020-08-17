2b2b65b599a02275fe482fec0893f68059e277c7
XuPeiYao <st89382000@gmail.com>
2020-08-17 22:11:43 +0800

    feat: 加入EFCore scaffold腳本
    (cherry picked from commit 94299539eba2b26fdea9bb935ce483c3d65c2cc4)

--- src/XPY.WebsiteSolution.Database/WebsiteSolutionContext.cs
+++ src/XPY.WebsiteSolution.Database/WebsiteSolutionContext.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace XPY.WebsiteSolution.Database
 {
-    /// <summary>
-    /// DAO
-    /// </summary>
-    public class WebsiteSolutionContext : DbContext
+    public partial class WebsiteSolutionContext : DbContext
     {
+        public WebsiteSolutionContext()
+        {
+        }
 
-        //public DbSet<SampleTableDao> SampleTableDao { get; set; } 
+        public WebsiteSolutionContext(DbContextOptions<WebsiteSolutionContext> options)
+            : base(options)
+        {
+        }
     }
 }
