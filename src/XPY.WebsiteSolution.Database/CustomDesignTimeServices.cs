b979e4e8a1b27890af296c57c5ea3b1439bf0a64
XuPeiYao <st89382000@gmail.com>
2020-08-17 22:11:43 +0800

    feat: 加入EFCore scaffold腳本
    (cherry picked from commit 94299539eba2b26fdea9bb935ce483c3d65c2cc4)

---
+++ src/XPY.WebsiteSolution.Database/CustomDesignTimeServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.WebsiteSolution.Database
+{
+    public class CustomDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ICSharpEntityTypeGenerator, CustomEntityTypeGenerator>();
+            serviceCollection.AddSingleton<ICSharpDbContextGenerator, CustomDbContextGenerator>();
+        }
+    }
+}
---
+++ src/XPY.WebsiteSolution.Database/CustomEntityTypeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace XPY.WebsiteSolution.Database
+{
+    public class CustomEntityTypeGenerator : CSharpEntityTypeGenerator
+    {
+        public CustomEntityTypeGenerator(ICSharpHelper csharpHelper) : base(csharpHelper)
+        { }
+
+        public override string WriteCode(IEntityType entityType, string @namespace, bool useDataAnnotations)
+        {
+            var classStr = base.WriteCode(entityType, @namespace, useDataAnnotations);
+
+            var defaultStr = "public partial class " + entityType.Name;
+            var baseStr = "public partial class " + entityType.Name + "Dao";
+
+            classStr = classStr.Replace(defaultStr, baseStr);
+
+            return classStr;
+        }
+    }
+}
