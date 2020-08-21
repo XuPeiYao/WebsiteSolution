4ae5e181378df4c523552c73a6b95b7d58df5158
XuPeiYao <st89382000@gmail.com>
2020-08-21 22:43:27 +0800

    fix: 恢復Cycle注入類別
    (cherry picked from commit 74037b8a0d9e5f369adc1ad00bca7d7857dca012)

--- src/XPY.WebsiteSolution.Web/Startup.cs
+++ src/XPY.WebsiteSolution.Web/Startup.cs
@@ -28,6 +28,7 @@ using XPY.WebsiteSolution.Database;
 using XPY.WebsiteSolution.Database.Pooling;
 using XPY.WebsiteSolution.Models;
 using XPY.WebsiteSolution.Services;
+using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.CycleDependent;
 using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.Injectable;
 using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.OpenApi;
 using XPY.WebsiteSolution.Utilities.Token;
@@ -113,6 +114,8 @@ namespace XPY.WebsiteSolution.Web
             services.AddMvc()
                 .AddControllersAsServices();
 
+            services.AddCycleDI();
+            
             //services.AddHealthChecks();
 
             services.AddAutoMapper(typeof(SampleUserModel));
