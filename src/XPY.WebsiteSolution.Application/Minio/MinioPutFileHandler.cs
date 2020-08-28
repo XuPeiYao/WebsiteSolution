b67f935c48af2d679935d3db2258e16c37a0d589
XuPeiYao <st89382000@gmail.com>
2020-08-29 00:47:37 +0800

    feat: 變更服務模式為中介者模式

---
+++ src/XPY.WebsiteSolution.Application/Minio/MinioPutFileHandler.cs
@@ -0,0 +1,68 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.Options;
+
+using Minio;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.Autofac;
+
+namespace XPY.WebsiteSolution.Application.Minio
+{
+    public class MinioPutFileRequest: IRequest<string>
+    {
+        public string ObjectName { get; set; }
+        public string ContentType { get; set; }
+        public Stream FileStream { get; set; }
+
+        public byte[] Binary {
+            get
+            {
+                throw new NotSupportedException();
+            }
+            set
+            {
+                FileStream = new MemoryStream(value);
+            }
+        }
+    }
+     
+    public class MinioPutFileHandler : IRequestHandler<MinioPutFileRequest, string>
+    {
+        public MinioClient Client { get; set; }
+
+        public IOptions<MinioOptions> Options { get; set; }
+
+        public MinioPutFileHandler(
+            MinioClient client,
+            IOptions<MinioOptions> options)
+        {
+            Client = client;
+            Options = options;
+        }
+
+        public async Task<string> Handle(MinioPutFileRequest request, CancellationToken cancellationToken)
+        {
+            await Client.PutObjectAsync(
+                Options.Value.BucketName,
+                request.ObjectName,
+                request.FileStream,
+                request.FileStream.Length,
+                request.ContentType);
+
+            if (request.ObjectName.StartsWith("shared_"))
+            {
+                var httpSchema = Options.Value.WithSSL ? "https" : "http";
+                return $"{httpSchema}://{Options.Value.EndPoint}/{Options.Value.BucketName}/{request.ObjectName}";
+            }
+
+            return await Client.PresignedGetObjectAsync(Options.Value.BucketName, request.ObjectName, 60 * 60 * 24);
+        }
+    }
+}
