b4378e88f75988dc68a1db532679bf3c7cc012e8
XuPeiYao <st89382000@gmail.com>
2020-08-29 00:47:37 +0800

    feat: 變更服務模式為中介者模式

---
+++ src/XPY.WebsiteSolution.Application/Minio/MinioGetFileHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.Options;
+
+using Minio;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.Autofac;
+
+namespace XPY.WebsiteSolution.Application.Minio
+{
+    public class MinioGetFileRequest : IRequest<string>
+    {
+        public string ObjectName { get; set; }
+    }
+
+    public class MinioGetFileHandler : IRequestHandler<MinioGetFileRequest, string>
+    {
+        public MinioClient Client { get; set; }
+
+        public IOptions<MinioOptions> Options { get; set; }
+
+        public MinioGetFileHandler(
+            MinioClient client,
+            IOptions<MinioOptions> options)
+        {
+            Client = client;
+            Options = options;
+        }
+
+        public async Task<string> Handle(MinioGetFileRequest request, CancellationToken cancellationToken)
+        {
+            if (request.ObjectName.StartsWith("shared_"))
+            {
+                var httpSchema = Options.Value.WithSSL ? "https" : "http";
+                return $"{httpSchema}://{Options.Value.EndPoint}/{Options.Value.BucketName}/{request.ObjectName}";
+            }
+
+            return await Client.PresignedGetObjectAsync(Options.Value.BucketName, request.ObjectName, 60 * 60 * 24);
+        }
+    }
+}
---
+++ src/XPY.WebsiteSolution.Application/Minio/MinioRemoveFileHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.Options;
+
+using Minio;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using XPY.WebsiteSolution.Utilities.Extensions.DependencyInjection.Autofac;
+
+namespace XPY.WebsiteSolution.Application.Minio
+{
+    public class MinioRemoveFileRequest : IRequest
+    {
+        public string ObjectName { get; set; }
+    }
+
+    public class MinioRemoveFileHandler : IRequestHandler<MinioRemoveFileRequest>
+    {
+        public MinioClient Client { get; set; }
+
+        public IOptions<MinioOptions> Options { get; set; }
+
+        public MinioRemoveFileHandler(
+            MinioClient client,
+            IOptions<MinioOptions> options)
+        {
+            Client = client;
+            Options = options;
+        }
+
+        public async Task<Unit> Handle(MinioRemoveFileRequest request, CancellationToken cancellationToken)
+        {
+            await Client.RemoveObjectAsync(
+                Options.Value.BucketName,
+                request.ObjectName);
+
+            return Unit.Value;
+        }
+    }
+}
