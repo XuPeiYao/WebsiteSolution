4acc131b519371393fe6345446ed946d1f93f5ab
XuPeiYao <st89382000@gmail.com>
2020-08-29 00:47:37 +0800

    feat: 變更服務模式為中介者模式

---
+++ src/XPY.WebsiteSolution.Application/SampleHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XPY.WebsiteSolution.Application
+{
+    public class SampleRequest : IRequest<string> {
+        public string Text { get; set; }
+    }
+
+    public class SampleHandler : IRequestHandler<SampleRequest, string>
+    {
+        public async Task<string> Handle(SampleRequest request, CancellationToken cancellationToken)
+        {
+            return request.Text;
+        }
+    }
+}
