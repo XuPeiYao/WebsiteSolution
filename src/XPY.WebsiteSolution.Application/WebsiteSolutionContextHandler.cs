e04a293b04838e3e0e181d97c63f6ef1f9edd653
XuPeiYao <st89382000@gmail.com>
2020-08-31 22:51:57 +0800

    feat: 更新Request Class

--- src/XPY.WebsiteSolution.Application/WebsiteSolutionContextHandler.cs
+++ src/XPY.WebsiteSolution.Application/WebsiteSolutionContextHandler.cs
@@ -12,13 +12,13 @@ using XPY.WebsiteSolution.Database;
 
 namespace XPY.WebsiteSolution.Application
 {
-    public class WebsiteSolutionRequest: IRequest<WebsiteSolutionContext>
+    public class WebsiteSolutionContextRequest: IRequest<WebsiteSolutionContext>
     {
 
     }
 
     public class WebsiteSolutionContextHandler :
-        IRequestHandler<WebsiteSolutionRequest, WebsiteSolutionContext>,
+        IRequestHandler<WebsiteSolutionContextRequest, WebsiteSolutionContext>,
         IDisposable
     {
         public ObjectPool<WebsiteSolutionContext> ContextPool { get; set; }
@@ -31,7 +31,7 @@ namespace XPY.WebsiteSolution.Application
             ContextPool = contextPool;
         }
 
-        public async Task<WebsiteSolutionContext> Handle(WebsiteSolutionRequest request, CancellationToken cancellationToken)
+        public async Task<WebsiteSolutionContext> Handle(WebsiteSolutionContextRequest request, CancellationToken cancellationToken)
         {
             if (_context != null)
             {
